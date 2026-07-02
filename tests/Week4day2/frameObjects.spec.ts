ff21e61bbac974f0bb074501a8d2bc6423479da8
PonnuSwamyGajendran <ponsamgs@gmail.com>
2026-07-02 21:56:52 +0530

    frameObjects

--- tests/Week4day2/frameObjects.spec.ts
+++ tests/Week4day2/frameObjects.spec.ts
@@ -1,3 +1,13 @@
+/* Frame Objects
+It will be handled either by URL or by name only
+Even if you have nested frame, you can directly access the inner frame using frame object with url/name
+Syntax:
+    const frameref = page.frame({url:'url link'})
+    await frameref?.locator('element locator').click
+    ? --> It will check whether frame is available or not 
+    If available, it will handle it
+    If frame is not present, it won't give any error and it returns undefined or null*/
+    
 import {test} from '@playwright/test'
 
 test('Frame using frame objects', async({page}) => {
